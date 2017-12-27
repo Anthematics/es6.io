2f672030a63e7a250fbeabb21b44eef6a743e7a7
Anthematics <jasonharder2@gmail.com>
2017-12-27 16:17:05 -0500

    fixed exercise so it only logs last item -> just had to remove from loop

---
+++ findlongestword.js
@@ -0,0 +1,11 @@
+function findLongestWord(str) {
+  longest = Array.of(str)
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+console.log(longest)function findLongestWord(str) {
+  longest = Array.of(str)
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+console.log(longest)
